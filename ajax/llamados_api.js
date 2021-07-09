da820e301e7b64aba65c3aeb4c41604167cb872e
rené rodriguez <reneclavijo@live.com>
2021-07-09 14:04:10 -0500

    agregando tarjetas de pokemon

--- ajax/llamados_api.js
+++ ajax/llamados_api.js
@@ -6,10 +6,20 @@ function buscarPokemon(){
     fetch("https://pokeapi.co/api/v2/pokemon/" + nombre_pokemon)
     .then(respuesta => respuesta.json())
     .then(datos => {
+
+    console.log(datos)
+
         nueva_etiqueta = `
-            <img src="${datos.sprites.front_default}">
+        <div class="card" style="width: 18rem;">
+            <img src="${datos.sprites.front_default}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${datos.name}</h5>
+                <p class="card-text">${datos.abilities[0].ability.name}</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
         `
-        $("#btn-buscar").after(nueva_etiqueta)
+        $("#contenedor-pokemones").after(nueva_etiqueta)
         //document.getElementById("btn-buscar").innerHTML += nueva_etiqueta
     })
     .catch(error => console.log(error))
